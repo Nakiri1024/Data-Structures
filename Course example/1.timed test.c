56885984574f7133f8637473444d6ffd841eea2f
Yuki <1597500653@qq.com>
2026-03-29 11:34:16 +0800

    add 1.timed test

---
+++ Course example/1.timed test.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<time.h>
+#include<math.h>
+
+const int N = 1000;// 多项式最大项数
+const int n = 1e7;// 计算的次数
+
+clock_t start, end;
+double duration;
+
+
+double f1(int n, double a[], double x);
+double f2(int n, double a[], double x);
+void run_f(double (*f)(int, double*, double), double a[], double x);
+
+int main()
+{
+    double a[N];// 多项式的系数
+    double x = 1.1;// 多项式的自变量
+    for(int i = 0; i < N; i++)
+        a[i] = (double)i;
+
+    run_f(f1, a, x);
+    run_f(f2, a, x);
+
+    return 0;
+}
+
+double f1(int n, double a[], double x)
+{
+    double p = a[0];
+    for(int i = 1; i <= n; i++){
+        p += a[i] * pow(x, i);
+    }
+    return p;
+}
+double f2(int n, double a[], double x)
+{
+    double p = a[n];
+    for(int i = n - 1; i >= 0; i--){
+        p = a[i] + x * p;
+    }
+    return p;
+}
+
+void run_f(double (*f)(int, double*, double), double a[], double x){
+    start = clock();
+    for(int i = 0; i < n; i++)
+        f(N -1, a, x);
+    end = clock();
+    duration = (double)(end - start) / CLK_TCK / n;
+    printf("ticks(%s)=%f\n", f == f1 ? "f1" : "f2", (double)(end - start));
+    printf("duration(%s)=%6.2e\n", f == f1 ? "f1" : "f2", duration);
+}
